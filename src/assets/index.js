8298946330ac2f5c900eae41f4a3a3971d604aa8
sivamani <101646636+saisivamani@users.noreply.github.com>
2024-05-30 15:28:30 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,67 @@
+// Logo
+import Icon from './logo/icon.png';
+
+// Technologies
+import htmlIcon from './tech/html.png';
+import cssIcon from './tech/css.png';
+import jsIcon from './tech/javascript.png';
+import reactIcon from './tech/reactjs.png';
+import awsIcon from './tech/aws.png';
+import javaIcon from './tech/java.png';
+import gitIcon from './tech/git.png';
+import githubIcon from './tech/github.png';
+import figmaIcon from './tech/figma.png';
+import psqlIcon from './tech/postgresql.png';
+import eslintIcon from './tech/eslint.png';
+import viteIcon from './tech/vite.svg';
+import pyIcon from './tech/py.png';
+import nodeIcon from './tech/node.webp';
+import neoIcon from './tech/neo.png';
+import raspIcon from './tech/raspi.png';
+import close from './close.svg';
+import menu from './menu.svg';
+
+// Projects
+import TicTacToe from './projects/tic_tac_toe.png';
+import boLunchBox from './projects/bo_lunch_box.png';
+import TakeHiro from './projects/takehiro.png';
+import AyushDrCafe from './projects/ayush.png';
+import prajwalan from './projects/prajwalan.jpg';
+import Startathon from './projects/startathon.jpeg';
+import StartUpExpo from './projects/startup_expo.jpg';
+import SihInternal from './projects/sih_internal.jpg';
+
+
+// Avatar
+import avatar from './avatar/avatar.png';
+
+export {
+  menu,
+  close,
+  htmlIcon,
+  cssIcon,
+  jsIcon,
+  reactIcon,
+  awsIcon,
+  javaIcon,
+  gitIcon,
+  githubIcon,
+  eslintIcon,
+  figmaIcon,
+  psqlIcon,
+  nodeIcon,
+  pyIcon,
+  viteIcon,
+  neoIcon,
+  raspIcon,
+  TicTacToe,
+  boLunchBox,
+  prajwalan,
+  Startathon,
+  SihInternal,
+  StartUpExpo,
+  TakeHiro,
+  AyushDrCafe,
+  avatar,
+  Icon,
+};
